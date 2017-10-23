79aec6da3069a1f633fee4f35ddd458e1ee8e2b1
Carlos A. Bonilla <lostineverland@gmail.com>
2017-10-22 22:30:08 -0400

    migrate to using usePackage

---
+++ init.js
@@ -0,0 +1,59 @@
+// Your init script
+
+// Atom will evaluate this file each time a new window is opened. It is run
+// after packages are loaded/activated and after the previous editor state
+// has been restored.
+
+// An example hack to log to the console when each text editor is saved.
+
+// atom.workspace.observeTextEditors (editor) ->
+//   editor.onDidSave ->
+//     console.log "Saved! #{editor.getPath()}"
+
+// This is being ruled by usePackage
+// https://github.com/bodil/atom-use-package
+const usePackage = require("atom-use-package").usePackage;
+
+atom.commands.add('atom-text-editor', 'user:multi-select', () => {
+  let editor = atom.workspace.getActiveTextEditor();
+  atom.commands.dispatch(atom.views.getView(editor), 'find-and-replace:select-next');
+  atom.commands.dispatch(atom.views.getView(editor), 'find-and-replace:use-selection-as-find-pattern');
+});
+
+usePackage("disable-keybindings", {
+  config: {
+    allCommunityPackages: true,
+    exceptCommunityPackages: []
+  }
+});
+
+// use case
+// usePackage('package-name', {
+//   config: {},
+//   keymap: {},
+//   init: {},
+//   enableKeys: false
+// }
+
+usePackage('Sublime-Style-Column-Selection', {enableKeys: false});
+usePackage('git-blame', {enableKeys: false});
+// usePackage('git-plus', {enableKeys: false});
+usePackage('hey-pane', {enableKeys: true});
+usePackage('highlight-selected', {enableKeys: false});
+usePackage('indentation-jumper', {enableKeys: true});
+usePackage('ink', {enableKeys: false});
+usePackage('lisp-paredit', {
+  keymap: {
+    '.platform-darwin atom-text-editor': {
+      'cmd-shift-space': 'lisp-paredit:expand-selection'
+    }
+  },
+  enableKeys: false
+});
+usePackage('parinfer', {enableKeys: true});
+usePackage('project-manager', {enableKeys: true});
+usePackage('proto-repl', {enableKeys: true});
+usePackage('proto-repl-charts', {enableKeys: false});
+usePackage('set-syntax', {enableKeys: false});
+// usePackage('sublime-word-navigation', {enableKeys: false});
+usePackage('word-jumper', {enableKeys: true});
